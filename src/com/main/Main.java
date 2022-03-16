1a1e0f1d9be44523aacb876b7b8bad9feab2c5f1
Sina Abdipoor <sinaalj@gmail.com>
2022-03-16 16:17:51 +0800

    Initial commit

---
+++ src/com/main/Main.java
@@ -0,0 +1,15 @@
+package com.main;
+
+/**
+ * This class is a test.
+ * <p>
+ * Created by Sina on 16-Mar-22
+ *
+ * @author Sina
+ * @version 0.1
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
