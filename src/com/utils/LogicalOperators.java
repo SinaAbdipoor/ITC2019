6eb6242230af86a2aa3875653dd9dabce247f54e
Sina Abdipoor <sinaalj@gmail.com>
2022-03-17 13:08:34 +0800

    LogicalOperators updated.

--- src/com/utils/LogicalOperators.java
+++ src/com/utils/LogicalOperators.java
@@ -8,50 +8,28 @@ package com.utils;
  * @author Sina
  * @version 0.1
  */
-class LogicalOperators {
+public class LogicalOperators {
 
     /**
-     * Performs logical and operation on the two given boolean arrays.
+     * Checks if boolean[] a is a subset of boolean[] b. a is a subset of b if and only if ((a or b) == a).
      *
      * @param a The first boolean array, boolean[] a.
-     * @param b The second boolean array, boolean[] b.
-     * @return The resulting boolean array, a and b.
+     * @param b b The second boolean array, boolean[] b.
+     * @return (( a or b) == a)?
      * @throws IllegalArgumentException If a.length != b.length.
      */
-    static boolean[] logicalAnd(boolean[] a, boolean[] b) throws IllegalArgumentException {
+    public static boolean isSubset(boolean[] a, boolean[] b) throws IllegalArgumentException {
         //TODO OPTIMIZATION: For faster running time, comment the following if. However, doing so will result in
-        // incorrect operation of this method. Make sure that the two given boolean arrays ALWAYS have same lengths
-        // before removing this check.
+        // incorrect operation of this method. Make sure that the given boolean arrays ALWAYS have same lengths before
+        // removing this check.
         if (a.length != b.length)
             throw new IllegalArgumentException("The two provided boolean arrays are of different lengths.");
         // COMMENT UNTIL HERE
 
-        boolean[] and = new boolean[a.length];
-        for (int i = 0; i < and.length; i++)
-            and[i] = a[i] && b[i];
-        return and;
-    }
-
-    /**
-     * Performs logical or operation on the two given boolean arrays.
-     *
-     * @param a The first boolean array, boolean[] a.
-     * @param b The second boolean array, boolean[] b.
-     * @return The resulting boolean array, a or b.
-     * @throws IllegalArgumentException If a.length != b.length.
-     */
-    static boolean[] logicalOr(boolean[] a, boolean[] b) throws IllegalArgumentException {
-        //TODO OPTIMIZATION: For faster running time, comment the following if. However, doing so will result in
-        // incorrect operation of this method. Make sure that the two given boolean arrays ALWAYS have same lengths
-        // before removing this check.
-        if (a.length != b.length)
-            throw new IllegalArgumentException("The two provided boolean arrays are of different lengths.");
-        // COMMENT UNTIL HERE
-
-        boolean[] or = new boolean[a.length];
-        for (int i = 0; i < or.length; i++)
-            or[i] = a[i] || b[i];
-        return or;
+        // Check if ((a or b) == a)
+        for (int i = 0; i < a.length; i++)
+            if ((a[i] || b[i]) != a[i]) return false;
+        return true;
     }
 
     /**
@@ -60,13 +38,13 @@ class LogicalOperators {
      *
      * @param a The first boolean array, boolean[] a.
      * @param b The second boolean array, boolean[] b.
-     * @return False if a&b.containt(true), and true otherwise.
-     * @throws IllegalArgumentException
+     * @return False if a&b.contains(true), and true otherwise.
+     * @throws IllegalArgumentException If a.length != b.length.
      */
-    static boolean andIsFalse(boolean[] a, boolean[] b) throws IllegalArgumentException {
+    public static boolean andIsFalse(boolean[] a, boolean[] b) throws IllegalArgumentException {
         //TODO OPTIMIZATION: For faster running time, comment the following if. However, doing so will result in
-        // incorrect operation of this method. Make sure that the two given boolean arrays ALWAYS have same lengths
-        // before removing this check.
+        // incorrect operation of this method. Make sure that the given boolean arrays ALWAYS have same lengths before
+        // removing this check.
         if (a.length != b.length)
             throw new IllegalArgumentException("The two provided boolean arrays are of different lengths.");
         // COMMENT UNTIL HERE
