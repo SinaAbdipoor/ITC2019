5eba7e3cdf11f6d7f2ee5be8179b142c7fcc5927
Sina Abdipoor <sinaalj@gmail.com>
2022-03-17 13:37:28 +0800

    areSubsets method in the LogicalOperators fixed.

--- src/com/utils/LogicalOperators.java
+++ src/com/utils/LogicalOperators.java
@@ -11,14 +11,15 @@ package com.utils;
 public class LogicalOperators {
 
     /**
-     * Checks if boolean[] a is a subset of boolean[] b. a is a subset of b if and only if ((a or b) == a).
+     * Checks if boolean[] a and boolean[] b are in a mutual subset relationship. a and b are subsets if and only if
+     * ((a or b) == a) or ((a or b) == b).
      *
      * @param a The first boolean array, boolean[] a.
      * @param b b The second boolean array, boolean[] b.
-     * @return (( a or b) == a)?
+     * @return (a or b) == a or (a or b) == b
      * @throws IllegalArgumentException If a.length != b.length.
      */
-    public static boolean isSubset(boolean[] a, boolean[] b) throws IllegalArgumentException {
+    public static boolean areSubsets(boolean[] a, boolean[] b) throws IllegalArgumentException {
         //TODO OPTIMIZATION: For faster running time, comment the following if. However, doing so will result in
         // incorrect operation of this method. Make sure that the given boolean arrays ALWAYS have same lengths before
         // removing this check.
@@ -26,9 +27,15 @@ public class LogicalOperators {
             throw new IllegalArgumentException("The two provided boolean arrays are of different lengths.");
         // COMMENT UNTIL HERE
 
-        // Check if ((a or b) == a)
-        for (int i = 0; i < a.length; i++)
-            if ((a[i] || b[i]) != a[i]) return false;
+        // Check if (a or b) == a or (a or b) == b
+        boolean[] superSet = null;
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] == b[i]) continue;
+            if (superSet == null) {
+                if (a[i]) superSet = a;
+                else superSet = b;
+            } else if (!superSet[i]) return false;
+        }
         return true;
     }
 
